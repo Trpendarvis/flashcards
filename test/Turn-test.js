8e72b6d0519681e1c5f61aeaca72e4b7c6638de0
Timothy Pendarvis <trpendarvis@gmail.com>
2023-02-02 18:53:01 -0700

    Add passing turn test

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -7,35 +7,45 @@ const Turn = require('../src/Turn.js')
 
 describe('Turn', function() {
 
-  it("should be a function and have two arguments", function() {
-    const turn = new Turn("test1", {"correctAnswer": "test1"})
-    expect(turn.userGuess).to.equal("test1")
-  }) 
+    it('returns the userGuess', () => {
+        const card = { question: 'What is 2 + 2?', correctAnswer: '4' }
+        const userGuess = '4'
+        const turn = new Turn(card, userGuess)
+        expect(turn.returnGuess()).to.equal(userGuess)
+    })
 
-  it("should have a card object for current card", function() {
-    const card = new Card(1,"test question",["answer1","answer2","answer3"],"correctAnswer1")
-    const turn = new Turn("correctAnswer1", {"correctAnswer": "correctAnswer1"})
-    expect(turn.userGuess).to.equal("correctAnswer1")
-  })
+    it('returns the card', () => {
+        const card = { question: 'What is 2 + 2?', correctAnswer: '4' }
+        const userGuess = '4'
+        const turn = new Turn(card, userGuess)
+        expect(turn.returnCard()).to.equal(card)
+    })
 
-  it("should have a method that returns the guess", function(){
-    const turn = new Turn("test2", {"id": 1, "correctAnswer": "test2"})
-    expect(turn.returnGuess()).to.deep.equal("test2")
-  })
+    it('returns true if userGuess is equal to the correctAnswer', () => {
+        const card = { question: 'What is 2 + 2?', correctAnswer: '4' }
+        const userGuess = '4'
+        const turn = new Turn(card, userGuess)
+        expect(turn.evaluateGuess()).to.equal(true)
+    })
 
-  it("should have a method that returns the card", function() {
-    const card = new Card(1,"test question",["answer1","answer2","answer3"],"correctAnswer1")
-    const turn = new Turn("correctAnswer1", card)
-    expect(turn.returnCard()).to.deep.equal(card)
-  })
+    it('returns false if userGuess is not equal to the correctAnswer', () => {
+        const card = { question: 'What is 2 + 2?', correctAnswer: '4' }
+        const userGuess = '5'
+        const turn = new Turn(card, userGuess)
+        expect(turn.evaluateGuess()).to.equal(false)
+    })
 
-  it("should have a method that returns a boolean for correct guess or not", function(){
-    const turn = new Turn("wrongGuess", {"id": 1, "correctAnswer": "correctGuess"})
-    expect(turn.evaluateGuess()).to.equal(false)
-  })
+    it('returns "Correct!" if evaluateGuess returns true', () => {
+        const card = { question: 'What is 2 + 2?', correctAnswer: '4' }
+        const userGuess = '4'
+        const turn = new Turn(card, userGuess)
+        expect(turn.giveFeedback()).to.equal('Correct!')
+    })
 
-  it("method that returns 'correct'/'incorrect' based on correctness of user guess", function(){
-    const turn = new Turn("correctGuess", {"id": 1, "question": "this is a test question", "answers": ["answer1","answer2","answer3"],"correctAnswer": "correctGuess"})
-    expect(turn.giveFeedback()).to.deep.equal("Correct!")
-  })
+    it('returns "Incorrect :(" if evaluateGuess returns false', () => {
+        const card = { question: 'What is 2 + 2?', correctAnswer: '4' }
+        const userGuess = '5'
+        const turn = new Turn(card, userGuess)
+        expect(turn.giveFeedback()).to.equal('Incorrect :(')
+    })
 })
